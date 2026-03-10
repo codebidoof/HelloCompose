b97e133a527a2a7a2d2e9232320b859221380cfd
princehw03 <princehw03@gmail.com>
2026-03-10 16:58:53 +0900

    refactor: 보일러플레이트 코드 삭제

--- app/src/main/java/kr/co/fastcampus/hello_world/practice/screens/ButtonEx.kt
+++ app/src/main/java/kr/co/fastcampus/hello_world/practice/screens/ButtonEx.kt
@@ -1,21 +1,8 @@
 package kr.co.fastcampus.hello_world.practice.screens
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Send
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import kr.co.fastcampus.hello_world.practice.components.Button0
 import kr.co.fastcampus.hello_world.practice.components.Button1
 import kr.co.fastcampus.hello_world.practice.components.Button2
--- app/src/main/java/kr/co/fastcampus/hello_world/practice/screens/SurfaceEx.kt
+++ app/src/main/java/kr/co/fastcampus/hello_world/practice/screens/SurfaceEx.kt
@@ -1,16 +1,8 @@
 package kr.co.fastcampus.hello_world.practice.screens
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import kr.co.fastcampus.hello_world.practice.components.Surface1
 import kr.co.fastcampus.hello_world.practice.components.Surface2
 import kr.co.fastcampus.hello_world.practice.components.Surface3
